8f40f56b7f5f97d8652072962eb4a99ef3aa1ede
elith95 <60621505+elith95@users.noreply.github.com>
2021-04-07 12:51:47 +0200

    Update R_time_series.R

--- R_time_series.R
+++ R_time_series.R
@@ -2,6 +2,11 @@ library(raster)
 
 setwd("Desktop/lab/greenland")
 
+#import raster
+lst_2000<-raster("lst_2000.tif")
 
-
+#import multiple raster
+files<-list.files(pattern="^lst")
+lst<-lapply(files,raster)
+lst_st<-stack(files)
 
