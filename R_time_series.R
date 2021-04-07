4c57c98784a4c07b5b819ace4acde5f377995f6b
elith95 <60621505+elith95@users.noreply.github.com>
2021-04-07 13:02:24 +0200

    Update R_time_series.R

--- R_time_series.R
+++ R_time_series.R
@@ -9,4 +9,5 @@ lst_2000<-raster("lst_2000.tif")
 files<-list.files(pattern="^lst")
 lst<-lapply(files,raster)
 lst_st<-stack(files)
+plotRGB(lst_st, 1, 2, 3, stretch="Lin") #RGB plot using the years of the time series -> keeps the highest values
 
